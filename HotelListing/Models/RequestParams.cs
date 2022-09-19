34484fb88c4772632fee5e732324a611530f25de
Andrew <andrew.morgan@e.org>
2022-09-19 11:24:18 +0100

    Implemented Paging on Get all countries

---
+++ HotelListing/Models/RequestParams.cs
@@ -0,0 +1,17 @@
+﻿namespace HotelListing.Models
+{
+    public class RequestParams
+    {
+        const int MaxPageSize = 150;
+
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+    }
+}
